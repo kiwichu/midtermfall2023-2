abf58af3e19d7183f01d1c33f04df58deeda8136
kiwichu <kiwichu365@gmail.com>
2023-10-01 19:31:05 -0400

    definitions complete

--- src/math/problems/Factorial.java
+++ src/math/problems/Factorial.java
@@ -2,6 +2,9 @@ package math.problems;
 
 public class Factorial {
 
+    public Factorial() {
+    }
+
     public static void main(String[] args) {
         /*
          * Factorial of 5! = 5 x 4 X 3 X 2 X 1 = 120.
@@ -9,5 +12,15 @@ public class Factorial {
          *
          */
 
+        /*
+        int num = 5;
+        long factorial = multiplyNumbers(num);
+        System.out.println("Factorial of " + num + " = " + factorial);
+
+
+    }
+}
+
+*/
     }
 }
--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -1,5 +1,7 @@
 package math.problems;
 
+import java.util.Scanner;
+
 public class Fibonacci {
     public static void main(String[] args) {
          /*
@@ -7,6 +9,22 @@ public class Fibonacci {
           0,1,1,2,3,5,8,13
          */
 
+        int first = 0;
+        int next = 1;
+        System.out.println("Enter how may fibonnaci numbers to print");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.print("The first " + n + " Fibonacci numbers are: ");
+        System.out.print("" + first + " " + next);
 
+
+        for (int i = 1; i <= n - 2; ++i) {
+            int sum = first + next;
+            first = next;
+            next = sum;
+            System.out.print(" " + sum);
+
+
+        }
     }
 }
